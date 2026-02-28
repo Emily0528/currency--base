2bbb0deb19b2261b5590fb91736dd68fd3a8ba20
Urszula Lukaszek <lu.urszula@gmail.com>
2026-02-28 19:36:17 +0100

    Fix convertPLNToUSD to pass new validation tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,14 +1,31 @@
 export const convertPLNToUSD = (PLN) => {
-  if (typeof PLN !== 'number' || isNaN(PLN)) {
+
+  if (PLN === undefined) {
     return NaN;
   }
 
-  const PLNtoUSD = PLN / 3.5;
-  
+  if (typeof PLN === 'string') {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  if (isNaN(PLN)) {
+    return NaN;
+  }
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
+
+  if (PLN < 0) {
+    return formatter.format(0).replace(/\u00a0/g, ' ');
+  }
+
+  const PLNtoUSD = PLN / 3.5;
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
